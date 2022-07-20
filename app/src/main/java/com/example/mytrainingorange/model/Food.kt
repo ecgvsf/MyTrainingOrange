9b5e550053d2df93b5f4d98a2cb4943e32591cec
Daniele Cazzo Lungo <daniin1303@gmail.com>
2022-07-20 10:03:20 +0200

    sad

--- app/src/main/java/com/example/mytrainingorange/model/Food.kt
+++ app/src/main/java/com/example/mytrainingorange/model/Food.kt
@@ -1,6 +1,6 @@
 package com.example.mytrainingorange.model
 
-class Food(val id : Int? = null, val name : String? = null) {
+class Food(val image : String? = null, val id : Int? = null, val name : String? = null) {
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
 }
